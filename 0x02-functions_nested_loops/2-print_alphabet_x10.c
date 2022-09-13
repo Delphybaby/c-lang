33968834e060660d3f730018aa51fb55dd874e5b
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-13 09:51:56 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  *
  */
 void print_alphabet_x10(void)
-{	
+{
 	char i;
 	char h;
 
 	for (h = 1; h <= 10; h++)
-	{ 
+	{
 		for (i = 'a'; i <= 'z'; i++)
-		{ 
+		{
 			_putchar(i);
 		}
 		_putchar('\n');
