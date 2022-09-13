6569e715c3034a18995e3f1127c11ce2d3c17721
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-13 05:46:08 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - prints alphabet
  *
- * Return: always return to 0
+ * Return: always 0
  *
  */
 void print_alphabet(void)
-{	for (char i = 'a'; i <= 'z'; i++)
-	
-	{	_putchar(i);
-	}
-		-putchar('\n');
-}
+{
+	char i;
 
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
