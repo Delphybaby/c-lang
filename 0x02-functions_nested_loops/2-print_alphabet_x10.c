6575d698a584414c35d433d8cada09e78bc5d4d3
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-13 06:06:45 -0700

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints alphabet 10 times
+ * print_alphabet_x10 - function to prints alphabet 10 times
  *
- * Return: always 0;
+ *
  *
  */
 void print_alphabet_x10(void)
