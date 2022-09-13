38357250c178809a99eab0737b1906432802db25
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-13 01:46:18 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - check the code
+ *
+ * Return: always return to 0
+ *
+ */
+int main(void)
+
+
+{	
+	for (char j = 1; j <= 10; j++)
+	{ for (char i = 'a'; i <= 'z'; i++)
+		{ putchar(i);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
