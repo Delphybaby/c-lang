176c0b641873aad594f8566284a1bedf446a3317
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-14 07:36:22 -0700

    3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,9 +12,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || year % 4 != 0) 
+	if (year % 4 == 0 ) 
 	{
-		if (month <= 12 && day <= 31)
+		if (month != 2 && day <= 31)
 		{
 			day++;
 		}
