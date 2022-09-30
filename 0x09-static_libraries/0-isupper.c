8de837b79bdd83d3b445ced65528eaac547f1fb3
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-30 05:20:49 -0700

    2

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *_isupper - changes to upper case
+ * @c: a function that converts to upper case
+ *
+ * Return: an upper case
+ */
+
+
+int _isupper(int c)
+{
+}
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+* _memset - fills a memory with constant byte
+*@s: memory space to be filled
+*@b: byte to fill with
+*@n: number of spaces to be filled
+*
+* Return: pointer to a memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+}
+
