0928508a0eb61f037d38b093bc1ef47767f2aa6d
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-30 07:39:34 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 /*
- *main- entry point
+ *main - Entry point
  *
  *Return: always 0
  */
-int main (int argc, char* argv[])
+int main(int argc, char  *argv[])
 {
 	int i;
-	for(i = 0; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("argv(%d): %s\n", i, argv[i]);
 	}
