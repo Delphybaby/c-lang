95ca9ed4a3ddd79b5c91f5a71d29d06e76bef138
Delphybaby <me.ejiofor@unizik.edu.ng>
2022-09-30 07:45:01 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-/*
- *main - Entry point
- *
- *Return: always 0
- */
-int main(int argc, char  *argv[])
-{
-	int i;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("argv(%d): %s\n", i, argv[i]);
-	}
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
 	return (0);
 }
